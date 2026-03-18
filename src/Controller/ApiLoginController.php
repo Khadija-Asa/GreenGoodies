b078f9da3addc649e7fe55a02a837b67eaab60e7
Khadidja <khadija.aitsiali@gmail.com>
2026-03-18 12:39:55 +0100

    done api with jwt + apiaccess check

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -4,14 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ApiLoginController extends AbstractController
+class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function index(): JsonResponse
     {
-        throw new \Exception('Should not be reached');
+        return new JsonResponse(null, 200);
     }
 }
